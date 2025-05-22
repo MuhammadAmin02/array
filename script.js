978e9733d0043a0cc907d57560925cb1ee0a8061
MuhammadAmin02 <muhammadamininto011@gmail.com>
2025-05-22 14:03:03 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,67 @@
+//let users = ['olma', 'nok', 'banan']
+
+//users.shift()
+//qusers.unshift()
+
+
+//console.log(users);
+
+//console.log(alert,a);
+
+
+
+//let users = ["olma" , "nok", "banan"];
+
+//alert(users)
+
+//let tasdiq = confirm("array qoshasizmi");
+
+
+
+
+//if (tasdiq) {
+//users.push{"kiwi"};
+//console.log(users);
+//alert(users);
+
+
+
+
+
+//else {
+
+    
+//    users.pop();
+  //  console.log(users);
+    //alert(users);
+    
+
+
+
+
+
+//}
+
+
+
+
+
+let users = ["olma", "nok","banan",];
+
+alert(users)
+
+
+let tasdiqlash = confirm("array qoshmoqchimisz");
+
+
+if (tasdiqlash) {
+   
+    users.push('kiwi');
+    console.log(users);
+    
+} else {
+    users.pop(); 
+    console.log(users);
+    
+}
+
